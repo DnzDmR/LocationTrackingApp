4fd9670807cdf1a0d0dae37dd82e96a03bd907c1
Denizcan <denizcandemir@outlook.com>
2019-09-14 12:37:17 +0300

    created login and home page

---
+++ source/screens/LoginScreens.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {View,Text} from 'react-native';
+
+export default class LoginScreens extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Login Page</Text>
+            </View>
+        )
+    }
+}
