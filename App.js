1d79859280c00280817a01bd60dd8c0d04f1103a
Denizcan <denizcandemir@outlook.com>
2019-09-17 19:12:23 +0300

    code review

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 
-import LoginScreen from './source/screens/LoginScreens';
-import HomeScreen from './source/screens/HomeScreens';
-import RegisterScreen from './source/screens/RegisterScreens';
+import LoginScreen from './source/screens/LoginScreen';
+import HomeScreen from './source/screens/HomeScreen';
+import RegisterScreen from './source/screens/RegisterScreen';
 
 const MainNavigator = createStackNavigator({
   Login: {screen: LoginScreen,navigationOptions:{header:null}},
