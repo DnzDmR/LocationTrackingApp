ab3e90afa51a0d68a7acb2ed8cbd9263decf5ebe
Denizcan <denizcandemir@outlook.com>
2019-09-18 19:51:24 +0300

    added google maps

--- source/screens/HomeScreen.js
+++ source/screens/HomeScreen.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
 import {View,Text} from 'react-native';
+import style from '../styles/loginStyle';
+import MapView, {Marker} from 'react-native-maps';
 
 export default class HomeScreens extends Component {
     render() {
         return (
-            <View>
-                <Text>Home Page</Text>
+            <View style={style.mapContainer}>
+                <MapView
+                    style={style.map}
+                    initialRegion={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}>
+
+
+
+                </MapView>
             </View>
         )
     }
--- source/styles/loginStyle.js
+++ source/styles/loginStyle.js
@@ -19,5 +19,22 @@ export default StyleSheet.create({
     formContainer:{
         width:"100%",
         top:100,    
-    }
+    },
+    mapContainer: {
+        position:'absolute',
+        top:0,
+        left:0,
+        right:0,
+        bottom:0,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+      },
+    map: {
+        position:'absolute',
+        top:0,
+        left:0,
+        right:0,
+        bottom:0,
+    },
+    
 });
